d386f16f82118b1c0ba8c7156267921d4b3a06a7
tajraab <tajrabacevac@gmail.com>
2023-12-19 18:00:27 +0100

    input-placeholder

---
+++ src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Input() {
+  return (
+    <div>
+        <div className="input-container">
+            <div className='name'>
+                <Input></Input>
+            </div>
+            <div className='email'></div>
+            <div className='password'></div>
+        </div>
+
+    </div>
+  )
+}
